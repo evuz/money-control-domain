e16cd73b0318c053c66b6911ec078bb24f7582a1
evuz <jesus_jgb_45@hotmail.com>
2019-02-08 09:53:02 +0100

    feat: export entities

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './database/UseCases/factory';
+export * from './database/Entities';
 export * from './users/UseCases/factory';
+export * from './users/Entities';
 export * from './activity/UseCases/factory';
+export * from './activity/Entities';
