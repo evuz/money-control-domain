59a36964da49dbc239a452b76bc352f5130524c2
evuz <jesus_jgb_45@hotmail.com>
2019-02-15 11:15:10 +0100

    feat: implemented ApiActivityRepository

---
+++ src/activity/Repositories/ApiActivityRepository.ts
@@ -0,0 +1,34 @@
+import { ActivityRepository, IGetActivitiesByMonth } from './ActivityRepository';
+import { ApiRepository } from '../../helpers/ApiRepository';
+import { Activity } from '../Entities/Activity';
+
+export class ApiActivityRepository extends ApiRepository implements ActivityRepository {
+  newActivity({ activity }: { activity: Activity }) {
+    const activityType = activity.amount > 0 ? 'income' : 'expense';
+    const endpoint = `activity/${activityType}`;
+    return this.http.post(endpoint, activity).then(res => res.data);
+  }
+
+  getActivity({ id }: { id: number }) {
+    return this.http.get(`activity/${id}`).then(res => res.data);
+  }
+
+  getActivities({  }: { user: Activity['user'] }): any {
+    throw Error('ApiActivityRepository#getActivities not implemented');
+  }
+
+  async getActivitiesByMonth({ user, date, take, page }: IGetActivitiesByMonth) {
+    return this.http
+      .get(`activities/${user}/${date}`, {
+        params: {
+          take,
+          page,
+        },
+      })
+      .then(res => res.data);
+  }
+
+  removeActivity({ id }: { id: string }): Promise<any> {
+    return this.http.delete(`activity/${id}`);
+  }
+}
